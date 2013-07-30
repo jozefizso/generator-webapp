47a8660046c60bae0c4cab9a49e8c85e1a19d654
Jozef Izso <jozef.izso@gmail.com>
2013-07-30 18:12:09 +0200

    Removed JS testing libraries, changed template directory structure. Changed grunt build file.

--- app/index.js
+++ app/index.js
@@ -8,19 +8,19 @@ var yeoman = require('yeoman-generator');
 var AppGenerator = module.exports = function Appgenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
-  // setup the test-framework property, Gruntfile template will need this
-  this.testFramework = options['test-framework'] || 'mocha';
+  // // setup the test-framework property, Gruntfile template will need this
+  // this.testFramework = options['test-framework'] || 'mocha';
 
-  // for hooks to resolve on mocha by default
-  if (!options['test-framework']) {
-    options['test-framework'] = 'mocha';
-  }
+  // // for hooks to resolve on mocha by default
+  // if (!options['test-framework']) {
+  //   options['test-framework'] = 'mocha';
+  // }
 
-  // resolved to mocha by default (could be switched to jasmine for instance)
-  this.hookFor('test-framework', { as: 'app' });
+  // // resolved to mocha by default (could be switched to jasmine for instance)
+  // this.hookFor('test-framework', { as: 'app' });
 
-  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
-  this.mainCoffeeFile = 'console.log "\'Allo from CoffeeScript!"';
+  // this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+  // this.mainCoffeeFile = 'console.log "\'Allo from CoffeeScript!"';
 
   this.on('end', function () {
     this.installDependencies({ skipInstall: options['skip-install'] });
@@ -36,38 +36,46 @@ AppGenerator.prototype.askFor = function askFor() {
 
   // welcome message
   console.log(this.yeoman);
-  console.log('Out of the box I include HTML5 Boilerplate, jQuery and Modernizr.');
+  console.log('Out of the box I include HTML5 Boilerplate, jQuery, Modernizr and Bootstrap CSS.');
 
-  var prompts = [{
+  var prompts = [/*{
     type: 'checkbox',
     name: 'features',
     message: 'What more would you like?',
     choices: [{
       name: 'Twitter Bootstrap for Sass',
       value: 'compassBootstrap',
-      checked: true
+      checked: false
     }, {
       name: 'RequireJS',
       value: 'includeRequireJS',
-      checked: true
+      checked: false
     }, {
       name: 'Autoprefixer for your CSS',
       value: 'autoprefixer',
-      checked: true
+      checked: false
     }]
-  }];
+  }*/];
 
   this.prompt(prompts, function (answers) {
-    var features = answers.features;
+    // var features = answers.features;
 
-    // manually deal with the response, get back and store the results.
-    // we change a bit this way of doing to automatically do this in the self.prompt() method.
-    this.compassBootstrap = features.indexOf('compassBootstrap') !== -1;
-    this.includeRequireJS = features.indexOf('includeRequireJS') !== -1;
-    this.autoprefixer = features.indexOf('autoprefixer') !== -1;
+    // // manually deal with the response, get back and store the results.
+    // // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    // this.compassBootstrap = features.indexOf('compassBootstrap') !== -1;
+    // this.includeRequireJS = features.indexOf('includeRequireJS') !== -1;
+    // this.autoprefixer = features.indexOf('autoprefixer') !== -1;
 
     cb();
   }.bind(this));
+};
+
+
+AppGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/js');
+  this.mkdir('app/css');
+  this.mkdir('app/img');
 };
 
 AppGenerator.prototype.gruntfile = function gruntfile() {
@@ -97,135 +105,89 @@ AppGenerator.prototype.editorConfig = function editorConfig() {
 };
 
 AppGenerator.prototype.h5bp = function h5bp() {
-  this.copy('favicon.ico', 'app/favicon.ico');
-  this.copy('404.html', 'app/404.html');
-  this.copy('robots.txt', 'app/robots.txt');
-  this.copy('htaccess', 'app/.htaccess');
-};
-
-AppGenerator.prototype.bootstrapImg = function bootstrapImg() {
-  if (this.compassBootstrap) {
-    this.copy('glyphicons-halflings.png', 'app/images/glyphicons-halflings.png');
-    this.copy('glyphicons-halflings-white.png', 'app/images/glyphicons-halflings-white.png');
-  }
-};
-
-AppGenerator.prototype.bootstrapJs = function bootstrapJs() {
-  // TODO: create a Bower component for this
-  if (this.compassBootstrap) {
-    this.copy('bootstrap.js', 'app/scripts/vendor/bootstrap.js');
-  }
+  this.copy('app/favicon.ico', 'app/favicon.ico');
+  this.copy('app/404.html', 'app/404.html');
+  this.copy('app/robots.txt', 'app/robots.txt');
+  this.copy('app/htaccess', 'app/.htaccess');
 };
 
 AppGenerator.prototype.mainStylesheet = function mainStylesheet() {
-  if (this.compassBootstrap) {
-    this.copy('main.scss', 'app/styles/main.scss');
-  } else {
-    this.copy('main.css', 'app/styles/main.css');
-  }
+  this.copy('app/site.css', 'app/css/site.css');
 };
 
-AppGenerator.prototype.writeIndex = function writeIndex() {
-  // prepare default content text
-  var defaults = ['HTML5 Boilerplate'];
-  var contentText = [
-    '        <div class="container">',
-    '            <div class="hero-unit">',
-    '                <h1>\'Allo, \'Allo!</h1>',
-    '                <p>You now have</p>',
-    '                <ul>'
-  ];
-
-  if (!this.includeRequireJS) {
-    this.indexFile = this.appendScripts(this.indexFile, 'scripts/main.js', [
-      'bower_components/jquery/jquery.js',
-      'scripts/main.js'
-    ]);
-
-    this.indexFile = this.appendFiles({
-      html: this.indexFile,
-      fileType: 'js',
-      optimizedPath: 'scripts/coffee.js',
-      sourceFileList: ['scripts/hello.js'],
-      searchPath: '.tmp'
-    });
-  }
-
-  if (this.compassBootstrap) {
-    defaults.push('Twitter Bootstrap');
-  }
-
-  if (this.compassBootstrap && !this.includeRequireJS) {
-    // wire Twitter Bootstrap plugins
-    this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
-      'bower_components/sass-bootstrap/js/bootstrap-affix.js',
-      'bower_components/sass-bootstrap/js/bootstrap-alert.js',
-      'bower_components/sass-bootstrap/js/bootstrap-dropdown.js',
-      'bower_components/sass-bootstrap/js/bootstrap-tooltip.js',
-      'bower_components/sass-bootstrap/js/bootstrap-modal.js',
-      'bower_components/sass-bootstrap/js/bootstrap-transition.js',
-      'bower_components/sass-bootstrap/js/bootstrap-button.js',
-      'bower_components/sass-bootstrap/js/bootstrap-popover.js',
-      'bower_components/sass-bootstrap/js/bootstrap-typeahead.js',
-      'bower_components/sass-bootstrap/js/bootstrap-carousel.js',
-      'bower_components/sass-bootstrap/js/bootstrap-scrollspy.js',
-      'bower_components/sass-bootstrap/js/bootstrap-collapse.js',
-      'bower_components/sass-bootstrap/js/bootstrap-tab.js'
-    ]);
-  }
-
-  if (this.includeRequireJS) {
-    defaults.push('RequireJS');
-  }
-
-  // iterate over defaults and create content string
-  defaults.forEach(function (el) {
-    contentText.push('                    <li>' + el  +'</li>');
-  });
-
-  contentText = contentText.concat([
-    '                </ul>',
-    '                <p>installed.</p>',
-    '                <h3>Enjoy coding! - Yeoman</h3>',
-    '            </div>',
-    '        </div>',
-    ''
-  ]);
-
-  // append the default content
-  this.indexFile = this.indexFile.replace('<body>', '<body>\n' + contentText.join('\n'));
+AppGenerator.prototype.mainJavaScript = function mainJavaScript() {
+  this.copy('app/main.js', 'app/js/main.js');
 };
 
-// TODO(mklabs): to be put in a subgenerator like rjs:app
-AppGenerator.prototype.requirejs = function requirejs() {
-  if (!this.includeRequireJS) {
-    return;
-  }
+// AppGenerator.prototype.writeIndex = function writeIndex() {
+//   // prepare default content text
+//   var defaults = ['HTML5 Boilerplate'];
+//   var contentText = [
+//     '        <div class="container">',
+//     '            <div class="hero-unit">',
+//     '                <h1>\'Allo, \'Allo!</h1>',
+//     '                <p>You now have</p>',
+//     '                <ul>'
+//   ];
 
-  this.indexFile = this.appendScripts(this.indexFile, 'scripts/main.js', ['bower_components/requirejs/require.js'], {
-    'data-main': 'scripts/main'
-  });
+//   if (!this.includeRequireJS) {
+//     this.indexFile = this.appendScripts(this.indexFile, 'scripts/main.js', [
+//       'bower_components/jquery/jquery.js',
+//       'scripts/main.js'
+//     ]);
 
-  // add a basic amd module
-  this.write('app/scripts/app.js', [
-    '/*global define */',
-    'define([], function () {',
-    '    \'use strict\';\n',
-    '    return \'\\\'Allo \\\'Allo!\';',
-    '});'
-  ].join('\n'));
+//     this.indexFile = this.appendFiles({
+//       html: this.indexFile,
+//       fileType: 'js',
+//       optimizedPath: 'scripts/coffee.js',
+//       sourceFileList: ['scripts/hello.js'],
+//       searchPath: '.tmp'
+//     });
+//   }
 
-  this.template('require_main.js', 'app/scripts/main.js');
-};
+//   if (this.compassBootstrap) {
+//     defaults.push('Twitter Bootstrap');
+//   }
 
-AppGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/scripts');
-  this.mkdir('app/styles');
-  this.mkdir('app/images');
-  this.write('app/index.html', this.indexFile);
-  this.write('app/scripts/hello.coffee', this.mainCoffeeFile);
-  if (!this.includeRequireJS) {
-    this.write('app/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
-  }
-};
+//   if (this.compassBootstrap && !this.includeRequireJS) {
+//     // wire Twitter Bootstrap plugins
+//     this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
+//       'bower_components/sass-bootstrap/js/bootstrap-affix.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-alert.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-dropdown.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-tooltip.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-modal.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-transition.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-button.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-popover.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-typeahead.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-carousel.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-scrollspy.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-collapse.js',
+//       'bower_components/sass-bootstrap/js/bootstrap-tab.js'
+//     ]);
+//   }
+
+//   if (this.includeRequireJS) {
+//     defaults.push('RequireJS');
+//   }
+
+//   // iterate over defaults and create content string
+//   defaults.forEach(function (el) {
+//     contentText.push('                    <li>' + el  +'</li>');
+//   });
+
+//   contentText = contentText.concat([
+//     '                </ul>',
+//     '                <p>installed.</p>',
+//     '                <h3>Enjoy coding! - Yeoman</h3>',
+//     '            </div>',
+//     '        </div>',
+//     ''
+//   ]);
+
+//   // append the default content
+//   this.indexFile = this.indexFile.replace('<body>', '<body>\n' + contentText.join('\n'));
+// };
+
+
