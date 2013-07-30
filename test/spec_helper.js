a78e409ccc065c53af9255524feffc7596a16649
Jozef Izso <jozef.izso@gmail.com>
2013-07-30 21:36:25 +0200

    Enhanced bower tests with proper JSON file parser.

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -2,8 +2,10 @@
 'use strict';
 
 var path    = require('path');
+var fs      = require('fs');
 var helpers = require('yeoman-generator').test;
-var assert = require('chai').assert;
+var assert  = require('chai').assert;
+var jsonlint = require("jsonlint");
 
 before(function (done) {
   helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
@@ -24,3 +26,17 @@ before(function (done) {
     });
   }.bind(this));
 });
+
+
+module.exports.jsonFile = function jsonFile (file) {
+  var here = fs.existsSync(file);
+  assert.ok(here, file + ', no such file or directory');
+
+  var body = fs.readFileSync(file, 'utf8');
+  try {
+    var obj = jsonlint.parse(body);
+    return obj;
+  } catch(e) {
+    assert.ok(false, file +' is not valid JSON file:\n'+ e.message);
+  }
+}
--- test/test-bower.js
+++ test/test-bower.js
@@ -5,27 +5,22 @@ var path    = require('path');
 var fs      = require('fs');
 var helpers = require('yeoman-generator').test;
 var assert  = require('chai').assert;
-var jsonlint = require("jsonlint");
-
-require('./spec_helper.js');
+var _       =require('./spec_helper.js');
 
 describe('Bower.json test', function () {
 
   it('creates .bowerrc file', function (done) {
-    var expected = [
-      ['.bowerrc', /"directory": "app\/bower_components"/]
-    ];
 
-    helpers.assertFiles(expected);
+    var bowerrc = _.jsonFile('.bowerrc');
+    assert.propertyVal(bowerrc, 'directory', 'app/bower_components');
+
     done();
   });
 
   it ('creates valid bower.json file', function (done) {
 
-    var body = fs.readFileSync('bower.json', 'utf8');
-    var bowerPkg = jsonlint.parse(body);
-
-    assert.isObject(bowerPkg, 'bower.json is valid object');
+    var bower = _.jsonFile('bower.json');
+    assert.propertyVal(bower, 'name', 'temp');
 
     done();
   });
